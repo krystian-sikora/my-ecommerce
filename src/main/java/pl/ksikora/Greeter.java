85d1f8b93bb992d23b69e55ff6da6e8836f33c78
Krystian Sikora <s223097@student.uek.krakow.pl>
2023-05-14 21:49:27 +0200

    plays with functional api

---
+++ src/main/java/pl/ksikora/Greeter.java
@@ -0,0 +1,7 @@
+package pl.ksikora;
+
+public class Greeter {
+    public void greet(String name){
+        System.out.printf("Hello %s%n", name);
+    }
+}
--- src/main/java/pl/ksikora/Main.java
+++ src/main/java/pl/ksikora/Main.java
@@ -1,7 +1,25 @@
 package pl.ksikora;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> names = Arrays.asList("Krystian", "Mikolaj", "Oliwia", "Jakub");
+        Greeter greeter = new Greeter();
+        greeter.greet("Krystian"); // -> Hello Krystian
+
+        List<String> ladies = new ArrayList<String>();
+        for (String name: names) {
+            if (name.endsWith("a")) {
+                ladies.add(name);
+            }
+        }
+
+        names.stream()
+                .filter(name -> name.endsWith("a"))
+                .forEach(greeter::greet);
+
     }
 }
