bf95d7f906ab8cb9f54479a4e65aa70d7cab5547
Krystian Sikora <s223097@student.uek.krakow.pl>
2023-05-15 17:22:40 +0200

    first spring integration test

---
+++ src/test/java/pl/ksikora/TestApp.java
@@ -0,0 +1,12 @@
+package pl.ksikora;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestApp {
+    @Test
+    public void contextLoads() {
+
+    }
+}
